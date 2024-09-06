8e76dd7fbe2955069758ef9f6d1c3e8092a1180b
Suryadip Sarkar <137927782+suryadip2008@users.noreply.github.com>
2024-09-06 23:07:42 +0530

    Update translate.js

--- networking/translate.js
+++ networking/translate.js
@@ -4,7 +4,8 @@ const text = process.argv[2];
 const targetLang = process.argv[3];
 
 translate(text, { to: targetLang }).then(res => {
-    console.log(res.text);
+    console.log(res.text);  // Print translated text
 }).catch(err => {
-    console.error(err);
+    console.error('Translation error:', err);
+    process.exit(1);  // Return error code if translation fails
 });
